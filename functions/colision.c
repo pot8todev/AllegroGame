2fa91ada3c8461eb98d4c20b4f479817ec8ae1eb
claudiorick06 <barcelosch06@gmail.com>
2025-05-27 09:26:56 -0300

    corrigindo alguns erros

--- functions/colision.c
+++ functions/colision.c
@@ -10,24 +10,24 @@ void colision(OBJETO *objeto, OBJETO *personagem) {
 
   // Retângulo do personagem
   float person_posx = personagem->posx, person_posy = personagem->posy;
-  float person_nova_posx = personagem->posx + personagem->vecVelocidade.dx;
-  float person_nova_posy = personagem->posy + personagem->vecVelocidade.dy;
+  float person_nova_posx = personagem->posx + personagem->vec_velocidade.dx;
+  float person_nova_posy = personagem->posy + personagem->vec_velocidade.dy;
 
   float person_w = personagem->sprite_w, person_h = personagem->sprite_h;
 
   // Retângulo da caixa
   float obj_posx = objeto->posx, obj_posy = objeto->posy;
-  float obj_w = objeto->sprite_w, obj_h = objeto->sprite_h;
+  float obj_w = objeto->sprite_w + 10, obj_h = objeto->sprite_h + 10;
 
   // SE o movimento acontecesse e causasse uma colisao:
   bool colidiux = person_nova_posy < obj_posy + obj_h / 2 &&
                   person_nova_posy + person_h / 2 > obj_posy &&
                   person_nova_posx < obj_posx + obj_w / 2 &&
-                  person_nova_posx + person_w / 2 > obj_posx;
+                  person_nova_posx + (person_w / 2) + 5 > obj_posx;
   if (colidiux) {
     // Inverte o vetor de movimento
-    personagem->vecVelocidade.dx = 0;
-    personagem->vecVelocidade.dy = 0;
+    personagem->vec_velocidade.dx = 0;
+    personagem->vec_velocidade.dy = 0;
   }
 }
 void limita_mapa(float *posx, float *posy, int maxdisplay_w, int maxdisplay_h,
--- main.c
+++ main.c
@@ -1,140 +1,141 @@
-    #include "functions/colision.h"
-    #include "functions/cria_mapa.h"
-    #include "functions/moving.h"
-    #include "structures/objeto.h"
-    #include <allegro5/allegro.h>
-    #include <allegro5/allegro_font.h>
-    #include <allegro5/allegro_image.h>
-    #include <allegro5/allegro_primitives.h>
-    #include <allegro5/allegro_ttf.h>
-    #include <allegro5/keyboard.h>
-    #include <stdio.h>
+#include "functions/colision.h"
+#include "functions/cria_mapa.h"
+#include "functions/moving.h"
+#include "structures/objeto.h"
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_ttf.h>
+#include <allegro5/keyboard.h>
+#include <stdio.h>
 
-    int main()
+int main()
+{
+    al_init();
+    al_install_keyboard();
+    al_init_image_addon();
+    al_init_font_addon();
+    al_init_ttf_addon();
+    al_init_primitives_addon();
+
+    int maxdisplay_w = 640;
+    int maxdisplay_h = 640;
+
+    ALLEGRO_DISPLAY *disp = al_create_display(maxdisplay_w, maxdisplay_h);
+    ALLEGRO_TIMER *timer =
+        al_create_timer(1.0 / 60.0); // TODO deixar mais generico
+    ALLEGRO_EVENT_QUEUE *queue = al_create_event_queue();
+    ALLEGRO_BITMAP *sprite = al_load_bitmap("images/sprites.png");
+    ALLEGRO_BITMAP *wooden_crate = al_load_bitmap("images/wooden_crate.png");
+
+    bool keys[ALLEGRO_KEY_MAX] = {0};
+
+    al_register_event_source(queue, al_get_display_event_source(disp));
+    al_register_event_source(queue, al_get_timer_event_source(timer));
+    al_register_event_source(queue, al_get_keyboard_event_source());
+
+    al_start_timer(timer);
+
+    ALLEGRO_EVENT event;
+
+    // --- Variaveis de jogo ---
+
+    OBJETO personagem = {sprite, 300, 200, {0, 0, 4.0}, 0, 32, 32, 4};
+    OBJETO wooden_crate_box = {wooden_crate, 60, 60, {0, 0, 0}, 0, 32, 32, 0};
+
+    float posxi, posyi;
+
+    // int dir = 0; // 0 = baixo, 1 = esquerda, 2 = cima, 3 = direita
+    int frame = 0;
+    int frame_counter = 0;
+
+    bool running = true;
+    bool moving = false;
+
+    // -------------------------
+
+    while (running)
     {
-        al_init();
-        al_install_keyboard();
-        al_init_image_addon();
-        al_init_font_addon();
-        al_init_ttf_addon();
-        al_init_primitives_addon();
+        al_wait_for_event(queue, &event);
 
-        int maxdisplay_w = 640;
-        int maxdisplay_h = 640;
+        // Eventos de teclado
+        if (event.type == ALLEGRO_EVENT_KEY_DOWN)
+            keys[event.keyboard.keycode] = true;
+        else if (event.type == ALLEGRO_EVENT_KEY_UP)
+            keys[event.keyboard.keycode] = false;
+        else if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+            running = false;
 
-        ALLEGRO_DISPLAY *disp = al_create_display(maxdisplay_w, maxdisplay_h);
-        ALLEGRO_TIMER *timer =
-            al_create_timer(1.0 / 60.0); // TODO deixar mais generico
-        ALLEGRO_EVENT_QUEUE *queue = al_create_event_queue();
-        ALLEGRO_BITMAP *sprite = al_load_bitmap("images/sprites.png");
-        ALLEGRO_BITMAP *wooden_crate = al_load_bitmap("images/wooden_crate.png");
+        // --- Lógica de movimento, direção e animação ---
 
-        bool keys[ALLEGRO_KEY_MAX] = {0};
-
-        al_register_event_source(queue, al_get_display_event_source(disp));
-        al_register_event_source(queue, al_get_timer_event_source(timer));
-        al_register_event_source(queue, al_get_keyboard_event_source());
-
-        al_start_timer(timer);
-
-        ALLEGRO_EVENT event;
-
-        // --- Variaveis de jogo ---
-
-        OBJETO personagem = {sprite, 300, 200, {0, 0, 4.0}, 0, 32, 32, 4};
-        OBJETO wooden_crate_box = {wooden_crate, 60, 60, {0, 0, 0}, 0, 32, 32, 0};
-
-        float posxi, posyi;
-
-        // int dir = 0; // 0 = baixo, 1 = esquerda, 2 = cima, 3 = direita
-        int frame = 0;
-        int frame_counter = 0;
-
-        bool running = true;
-        bool moving = false;
-
-        // -------------------------
-
-        while (running)
+        if (event.type == ALLEGRO_EVENT_TIMER)
         {
-            al_wait_for_event(queue, &event);
 
-            // Eventos de teclado
-            if (event.type == ALLEGRO_EVENT_KEY_DOWN)
-                keys[event.keyboard.keycode] = true;
-            else if (event.type == ALLEGRO_EVENT_KEY_UP)
-                keys[event.keyboard.keycode] = false;
-            else if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
-                running = false;
+            personagem.vec_velocidade.dx = 0;
+            personagem.vec_velocidade.dy = 0;
+            moving = false;
 
-            // --- Lógica de movimento, direção e animação ---
+            moving_test_up(keys[ALLEGRO_KEY_UP], &moving, &personagem);
+            moving_test_down(keys[ALLEGRO_KEY_DOWN], &moving, &personagem);
+            moving_test_right(keys[ALLEGRO_KEY_RIGHT], &moving, &personagem);
+            moving_test_left(keys[ALLEGRO_KEY_LEFT], &moving, &personagem);
 
-            if (event.type == ALLEGRO_EVENT_TIMER)
+            moving_test_up(keys[ALLEGRO_KEY_W], &moving, &personagem);
+            moving_test_down(keys[ALLEGRO_KEY_S], &moving, &personagem);
+            moving_test_right(keys[ALLEGRO_KEY_D], &moving, &personagem);
+            moving_test_left(keys[ALLEGRO_KEY_A], &moving, &personagem);
+
+            limita_mapa(&personagem.posx, &personagem.posy, maxdisplay_w,
+                        maxdisplay_h, personagem.sprite_w, personagem.sprite_h);
+
+            // frame loop
+            if (moving)
             {
+                // aplicaçao do incremento
+                normal_vetor(&personagem);
+                colision(&wooden_crate_box, &personagem);
+                personagem.posx += personagem.vec_velocidade.dx;
+                personagem.posy += personagem.vec_velocidade.dy;
+                // normalizacao vetor diagonal
 
-                personagem.vec_velocidade.dx = 0;
-                personagem.vec_velocidade.dy = 0;
-                moving = false;
-
-                moving_test_up(keys[ALLEGRO_KEY_UP], &moving, &personagem);
-                moving_test_down(keys[ALLEGRO_KEY_DOWN], &moving, &personagem);
-                moving_test_right(keys[ALLEGRO_KEY_RIGHT], &moving, &personagem);
-                moving_test_left(keys[ALLEGRO_KEY_LEFT], &moving, &personagem);
-
-                moving_test_up(keys[ALLEGRO_KEY_W], &moving, &personagem);
-                moving_test_down(keys[ALLEGRO_KEY_S], &moving, &personagem);
-                moving_test_right(keys[ALLEGRO_KEY_D], &moving, &personagem);
-                moving_test_left(keys[ALLEGRO_KEY_A], &moving, &personagem);
-
-                limita_mapa(&personagem.posx, &personagem.posy, maxdisplay_w,
-                            maxdisplay_h, personagem.sprite_w, personagem.sprite_h);
-
-                // frame loop
-                if (moving)
+                // TODO fps func
+                frame_counter++;
+                if (frame_counter >= 10)
                 {
-                    // aplicaçao do incremento
-                    normal_vetor(&personagem);
-                    colision(&wooden_crate_box, &personagem);
-        personagem.posx += personagem.vec_velocidade.dx;
-                    personagem.posy += personagem.vec_velocidade.dy;
-                    // normalizacao vetor diagonal
-
-                    // TODO fps func
-                    frame_counter++;
-                    if (frame_counter >= 10)
-                    {
-                        frame = (frame + 1) % personagem.num_frames;
-                        frame_counter = 0;
-                    }
-
-      } else {
-        frame = 0; // Parado: usa quadro do meio
-      }
-      al_clear_to_color(al_map_rgb(0, 0, 0));
-      al_draw_bitmap_region(wooden_crate_box.sprite, 0, 0,
-                            wooden_crate_box.sprite_w,
-                            wooden_crate_box.sprite_h, wooden_crate_box.posx,
-                            wooden_crate_box.posy, 0);
-      al_draw_bitmap_region(sprite, frame * personagem.sprite_w,
-                            personagem.sprite_dir * personagem.sprite_h,
-                            personagem.sprite_w, personagem.sprite_h,
-                            personagem.posx, personagem.posy, 0);
-      al_draw_bitmap_region(wooden_crate_box.sprite, 0, 0,
-                            wooden_crate_box.sprite_w,
-                            wooden_crate_box.sprite_h * 0.5,
-                            wooden_crate_box.posx, wooden_crate_box.posy, 0);
-      cria_mapa("images/dados.txt", wooden_crate_box);
-
-                al_flip_display();
+                    frame = (frame + 1) % personagem.num_frames;
+                    frame_counter = 0;
+                }
             }
-            // ------------------------------------------------
+            else
+            {
+                frame = 0; // Parado: usa quadro do meio
+            }
+            al_clear_to_color(al_map_rgb(0, 0, 0));
+            al_draw_bitmap_region(wooden_crate_box.sprite, 0, 0,
+                                  wooden_crate_box.sprite_w,
+                                  wooden_crate_box.sprite_h, wooden_crate_box.posx,
+                                  wooden_crate_box.posy, 0);
+            al_draw_bitmap_region(sprite, frame * personagem.sprite_w,
+                                  personagem.sprite_dir * personagem.sprite_h,
+                                  personagem.sprite_w, personagem.sprite_h,
+                                  personagem.posx, personagem.posy, 0);
+            al_draw_bitmap_region(wooden_crate_box.sprite, 0, 0,
+                                  wooden_crate_box.sprite_w,
+                                  wooden_crate_box.sprite_h * 0.5,
+                                  wooden_crate_box.posx, wooden_crate_box.posy, 0);
+            cria_mapa("images/dados.txt", wooden_crate_box);
+
+            al_flip_display();
         }
-        
-        // Limpeza
-        al_destroy_bitmap(sprite);
-        al_destroy_bitmap(wooden_crate);
-        al_destroy_timer(timer);
-        al_destroy_event_queue(queue);
-        al_destroy_display(disp);
-        return 0;
+        // ------------------------------------------------
     }
+
+    // Limpeza
+    al_destroy_bitmap(sprite);
+    al_destroy_bitmap(wooden_crate);
+    al_destroy_timer(timer);
+    al_destroy_event_queue(queue);
+    al_destroy_display(disp);
+    return 0;
+}
